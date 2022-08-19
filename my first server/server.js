9fd8bc82d6074cb9e365ac4e24b724eefc3e8887
Aditya-raj77x <ar8342077@gmail.com>
2022-08-19 00:16:59 -0400

    Create server.js

---
+++ my first server/server.js
@@ -0,0 +1,9 @@
+const http=require("http");
+const port=8081;
+http.createServer((request,response)=>{
+    response.writeHead(200,{"Content-Type":"text/html"});
+    response.write("<h1>THIS IS ADITYA'S FIRST EVEN SERVER</h1>");
+    response.end();
+}).listen(port,()=>{
+    console.log(`ADITYA's SERVER STARTED AT PORT ${port}`);
+});
