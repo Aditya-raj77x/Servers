f5239efaf3b050fb8b315cad07d19264076d5db3
Aditya-raj77x <ar8342077@gmail.com>
2022-08-19 23:18:14 -0400

    added node mon

--- my first server/server.js
+++ my first server/server.js
@@ -2,7 +2,7 @@ const http=require("http");
 const port=8081;
 http.createServer((request,response)=>{
     response.writeHead(200,{"Content-Type":"text/html"});
-    response.write("<h1>THIS IS ADITYA'S FIRST EVEN SERVER</h1>");
+    response.write("<h1>THIS IS ADITYA'S FIRST EVEN SERVER</h1> <h1>DAY TWO TRY RUN</h1>");
     response.end();
 }).listen(port,()=>{
     console.log(`ADITYA's SERVER STARTED AT PORT ${port}`);
