e80df859b3cde383b546a77955917a40c947fced
Aditya-raj77x <ar8342077@gmail.com>
2022-08-21 23:59:26 -0400

    added get push

--- my first server/server.js
+++ my first server/server.js
@@ -1,9 +1,35 @@
-const http=require("http");
-const port=8081;
-http.createServer((request,response)=>{
-    response.writeHead(200,{"Content-Type":"text/html"});
-    response.write("<h1>THIS IS ADITYA'S FIRST EVEN SERVER</h1> <h1>DAY TWO TRY RUN</h1>");
-    response.end();
-}).listen(port,()=>{
+const http = require("http");
+const port = 8081;
+const doList = ["gaming", "chating", "music"];
+http
+  .createServer((request, response) => {
+    const { method, url } = request;
+    if(url==="/doList"){
+        if(method==="GET"){
+            response.writeHead(200, {"Content-Type": "text/html"} );
+            response.write(doList.toString());
+            
+        }else if(method==="POST"){
+            let body="";
+            request.on('error',(err)=>{
+                console.error(err);
+            }).on('data',(ck)=>{
+                body+=ck;
+            }).on('end',()=>{
+                body=JSON.parse(body);
+                console.log("DATA :",body);
+            })
+        }
+
+        else{
+            response.writeHead(502);
+            
+        }
+    }else{
+        response.writeHead(404);
+    }
+    response.end()
+  })
+  .listen(port, () => {
     console.log(`ADITYA's SERVER STARTED AT PORT ${port}`);
-});
+  });
